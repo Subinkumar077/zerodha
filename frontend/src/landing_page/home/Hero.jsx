59ed17e5c804805680511fd08e24b6ab5aa66a98
Subin Kumar <sadiesink718@gmail.com>
2026-06-22 18:58:29 +0530

    landing-page-structure

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,7 +1,14 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+      <div className="container">
+        <div className="row">
+          <img src="media/images/homeHero.png" alt="Hero image" />
+          h1 
+        </div>
+      </div>
+    </div>
   )
 }
 
