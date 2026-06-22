067b7100533f88332cfa55843e29a72be68c8826
Subin Kumar <sadiesink718@gmail.com>
2026-06-22 19:23:08 +0530

    landing-page-structure

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -5,7 +5,9 @@ const Hero = () => {
       <div className="container">
         <div className="row">
           <img src="media/images/homeHero.png" alt="Hero image" />
-          h1 
+          <h1>Invest in everything</h1>
+          <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+          <button>Sign up for free</button>
         </div>
       </div>
     </div>
